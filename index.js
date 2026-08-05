f99fd262babb065b039946bbc81c22f0b4911bfc
Bell914 <ittikorn.tipson@gmail.com>
2026-08-05 13:06:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+class Animal {
+    constructor(name,species){
+        this.name =name;
+        this.species = species;
+        this.hunger = 50;
+
+    }
+    makeSound(){
+        console.log(`${this.name} makes a sound .... `);
+    }
+    eat(){
+        this.hunger = this.hunger - 10;
+        console.log(
+            `${this.name} ate, Hunger level is now ${this.hunger}.`,
+        );
+        }    
+    } 
+    //opject instans
+const leo = new Animal("Leo","Lion");
+console.log(leo);
+
+console.log(leo.hunger);
+leo.eat();
+console.log(leo.hunger);
+
+// specialized class(Inhertance)
+
+class Mammal extends Animal{
+    constructor(name,species,furColor){
+        this.furColor = furColor;
+
+    }
+    groom(){
+        console.log(`${this.name} is brushing their ${this.furColor} fur.`)
+    }
+}
+class Bird extends Animal{
+    constructor(name,species,wingSpan){
+        this.wingSpan = wingSpan;
+    }
+// this is an example of Polymorphism; overriding the makeSound method
+    makeSound(){
+        console.log(`${this.name} chirps: Tweet tweet!`);
+    }
+}
