b18653e82158da6f8fa03a86fdf7f654acd6c988
Bell914 <ittikorn.tipson@gmail.com>
2026-08-07 22:00:07 +0700

    update change

--- first-meet-fetch.js
+++ first-meet-fetch.js
@@ -1,5 +1,7 @@
 // const url = "https://jsonplaceholder.typicode.com/posts/1";
-const url ="https://api.api-ninjas.com/v1/animals/posts/1"
+// const url = "https://bored-api.appbrewery.com/random" // random bored API
+// const url ="https://api.api-ninjas.com/v1/animals/posts/1"
+const url = "https://catfact.ninja/fact"; // random cat facts
 async function getPost() {
   try {
     const response = await fetch(url);
